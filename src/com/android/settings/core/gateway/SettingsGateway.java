0df805d8e4522cd92fff3f9853411fcbb19c3034
Pranav Vashi <neobuddy89@gmail.com>
2023-03-27 18:26:58 +0530

    Settings: Add ambient Edge Light Settings
    Change-Id: Ia40383b6d1ec7b8b940f6eef1f884dfcd0e76c37
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -23,6 +23,7 @@ import co.aospa.settings.gestures.PowerButtonTorchSettings;
 import co.aospa.settings.sound.AdaptivePlaybackSoundSettings;
 import co.aospa.settings.statusbar.NetworkTrafficSettings;
 import co.aospa.settings.powermenu.PowerMenuActions;
+import co.aospa.settings.edgelight.EdgeLightSettings;
 
 import com.android.settings.AllInOneTetherSettings;
 import com.android.settings.DisplaySettings;
@@ -360,6 +361,7 @@ public class SettingsGateway {
             AdaptivePlaybackSoundSettings.class.getName(),
             NetworkTrafficSettings.class.getName(),
             PowerMenuActions.class.getName(),
+            EdgeLightSettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             StorageDashboardFragment.class.getName(),
